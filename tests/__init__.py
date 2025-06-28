607bb679ff1b49b7469f7f07b83a78d90dc3831c
LysenkovIlya-Hexlet <lysenkov_iv21@mail.ru>
2025-06-28 22:29:32 +0300

    Пофиксил ошибки линтера

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,7 @@
 import os.path
 __all__ = ["build_fixture_path"]
-FIXTURES_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "fixtures")
+current_dir = os.path.dirname(os.path.abspath(__file__))
+FIXTURES_PATH = os.path.join(current_dir, "fixtures")
 
 
 def build_fixture_path(file, path=FIXTURES_PATH):
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,6 +3,11 @@ import pytest
 from tests import test_formaters
 
 
-@pytest.mark.parametrize("file_name1,file_name2", [("file1.json", "file1.yaml"), ("file2.json", "file2.yml")])
+@pytest.mark.parametrize(
+    "file_name1,file_name2",
+    [("file1.json", "file1.yaml"), ("file2.json", "file2.yml")]
+)
 def test_pars(file_name1, file_name2):
-    assert parser.get_content(test_formaters.build_fixture_path(file_name1)) == parser.get_content(test_formaters.build_fixture_path(file_name2))
+    content1 = parser.get_content(test_formaters.build_fixture_path(file_name1))
+    content2 = parser.get_content(test_formaters.build_fixture_path(file_name2))
+    assert content1 == content2
