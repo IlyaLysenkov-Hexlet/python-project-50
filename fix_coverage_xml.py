77fc7358a9b77fa4ab669ea5f437c72f51ecbfdd
LysenkovIlya-Hexlet <lysenkov_iv21@mail.ru>
2025-07-08 17:49:33 +0300

    tired

--- fix_coverage_xml.py
+++ fix_coverage_xml.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 
+
 def fix_coverage_xml(path_in='coverage.xml', path_out='coverage_fixed.xml'):
     tree = ET.parse(path_in)
     root = tree.getroot()
@@ -8,6 +9,7 @@ def fix_coverage_xml(path_in='coverage.xml', path_out='coverage_fixed.xml'):
         for source in sources.findall('source'):
             source.text = 'gendiff'
     tree.write(path_out)
+
 
 if __name__ == '__main__':
     fix_coverage_xml()
